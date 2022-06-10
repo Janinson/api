96d6b0693751f46db859e2c120a3eec34155da71
Janinson <yaninhurco@hotmail.com>
2022-06-09 21:18:32 -0500

    se añade informacion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const req = require('express/lib/request');
 
 const app = express();
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+app.get('api/libros', (req, res) => res.json({
+    nombre:"prueba"
+}))
 
 // Routes
 app.use(require('./routes/index'));
