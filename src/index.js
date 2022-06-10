23e6175043aa3415a3e4b3c2de6819a351d3cca6
Janinson <yaninhurco@hotmail.com>
2022-06-09 21:28:12 -0500

    cambio de puerto index

--- src/index.js
+++ src/index.js
@@ -7,13 +7,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('api/libros', (req, res) => res.json({
-    nombre:"prueba"
-}))
 
 // Routes
 app.use(require('./routes/index'));
 
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
+app.listen(port, host, function() {
+    console.log("Server started......." + port);
+  });
+
+  /*
 app.listen(process.env.PORT || 3000)
 console.log('Server on port', 3000);
+
+*/
