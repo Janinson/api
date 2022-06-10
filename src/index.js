4e1575f754b9ee3123d99f11eb265fdb187423fb
Janinson <yaninhurco@hotmail.com>
2022-06-09 21:30:49 -0500

    cambio

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.get('api/libros', (req, res) => res.json({
+    nombre:"prueba"
+}))
 
 // Routes
 app.use(require('./routes/index'));
